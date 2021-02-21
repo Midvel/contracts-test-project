f27eec735d6ccbc2eaa830a5d6acbc3ae770aed1
Midvel <midvel.corp@gmail.com>
2021-02-21 19:35:27 +0200

    Docgen added

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,6 +9,11 @@ contract MyToken is ERC20 {
         _setupDecimals(2);
     }
 
+    /**
+     * @notice Mints desired amount of tokens for the recepient
+     * @param _receiver Receiver of the tokens.
+     * @param _amount Amount (in wei - smallest decimals)
+     */
     function mintFor(address _receiver, uint256 _amount) external {
         require(_receiver != address(0), "Zero address");
         require(_receiver != address(this), "Incorrect address");
